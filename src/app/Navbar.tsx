51bff144e5d5023b7a9483e6ad7a043143fa4cdb
railsfactory-robin <robinthomas@railsfactory.org>
2024-03-08 12:11:21 +0530

    created issue modal

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import { AiFillBug } from "react-icons/ai";
+import classNames from 'classnames';
+
+function Navbar() {
+  const currentPath = usePathname()
+  const links = [
+    { label: 'Dashboard', href: "/" },
+    { label: 'Issues', href: "/issues" },
+  ]
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+      <Link href="/"><AiFillBug /></Link>
+      <ul className='flex space-x-6'>
+        {links.map(link => <Link
+          className={classNames({
+            'text-zinc-900': link.href === currentPath,
+            'text-zinc-500': link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors': true
+          })}
+          key={link.href}
+          href={link.href}>
+          {link.label}
+        </Link>)}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
